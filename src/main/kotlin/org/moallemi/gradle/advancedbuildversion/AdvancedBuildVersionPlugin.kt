75de6d77ae65b418eb630c2cad8e49cb4f23abe1
Reza Moallemi <rm.allmails@gmail.com>
2020-03-15 21:58:48 +0100

    Rewrite file output options with kotlin

--- src/main/kotlin/org/moallemi/gradle/advancedbuildversion/AdvancedBuildVersionPlugin.kt
+++ src/main/kotlin/org/moallemi/gradle/advancedbuildversion/AdvancedBuildVersionPlugin.kt
@@ -1,17 +1,33 @@
 package org.moallemi.gradle.advancedbuildversion
 
+import com.android.build.gradle.AppExtension
+import com.android.build.gradle.AppPlugin
+import com.android.build.gradle.LibraryPlugin
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.internal.impldep.org.eclipse.jgit.errors.NotSupportedException
 import org.moallemi.gradle.advancedbuildversion.gradleextensions.AdvancedBuildVersionConfig
 
 class AdvancedBuildVersionPlugin : Plugin<Project> {
 
     override fun apply(project: Project) {
-        val plugin = project.extensions.create(
+        val advancedBuildVersionPlugin = project.extensions.create(
             "advancedVersioning", AdvancedBuildVersionConfig::class.java, project
         )
+
         project.afterEvaluate {
-            plugin.versionCodeConfig.increaseVersionCodeIfPossible()
+            project.plugins.all { plugin ->
+                when (plugin) {
+                    is AppPlugin -> configureAndroid(project, advancedBuildVersionPlugin)
+                    is LibraryPlugin -> throw NotSupportedException("Android library module is not supported yet")
+                }
+            }
         }
     }
+
+    private fun configureAndroid(project: Project, config: AdvancedBuildVersionConfig) =
+        with(project.extensions.getByType(AppExtension::class.java)) {
+            config.versionCodeConfig.increaseVersionCodeIfPossible()
+            config.outputConfig.renameOutputApkIfPossible(applicationVariants)
+        }
 }
---
+++ src/main/kotlin/org/moallemi/gradle/advancedbuildversion/gradleextensions/FileOutputConfig.kt
@@ -0,0 +1,59 @@
+package org.moallemi.gradle.advancedbuildversion.gradleextensions
+
+import com.android.build.gradle.api.ApplicationVariant
+import com.android.build.gradle.internal.api.BaseVariantOutputImpl
+import groovy.text.SimpleTemplateEngine
+import org.gradle.api.DomainObjectSet
+import org.gradle.api.Project
+
+class FileOutputConfig(private val project: Project) {
+
+    private var nameFormat: String? = null
+
+    private var renameOutput = false
+
+    private val templateEngine: SimpleTemplateEngine = SimpleTemplateEngine()
+
+    fun nameFormat(format: String) {
+        nameFormat = format
+    }
+
+    fun renameOutput(b: Boolean) {
+        renameOutput = b
+    }
+
+    fun renameOutputApkIfPossible(variants: DomainObjectSet<ApplicationVariant>) {
+        if (renameOutput) {
+            variants.all { variant ->
+                generateOutputName(variant)
+            }
+        }
+    }
+
+    private fun generateOutputName(variant: ApplicationVariant) {
+        val map = linkedMapOf(
+            "appName" to project.name,
+            "projectName" to project.rootProject.name,
+            "flavorName" to variant.flavorName,
+            "buildType" to variant.buildType.name,
+            "versionName" to (variant.versionName ?: ""),
+            "versionCode" to variant.versionCode.toString()
+        )
+
+        val template = nameFormat ?: variant.flavorName
+            ?.takeIf { it.isNotBlank() }
+            ?.let {
+                "\$appName-\$flavorName-\$buildType-\$versionName"
+            } ?: "\$appName-\$buildType-\$versionName"
+
+        val fileName = templateEngine
+            .createTemplate(template)
+            .make(map)
+            .toString()
+
+        variant.outputs.all { output ->
+            val outputImpl = output as BaseVariantOutputImpl
+            outputImpl.outputFileName = "$fileName.apk"
+        }
+    }
+}
