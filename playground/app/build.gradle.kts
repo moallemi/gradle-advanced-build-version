ab373d83182f143511d522f4c83f779ae7342916
Reza Moallemi <rm.allmails@gmail.com>
2026-01-18 11:37:41 -0800

    Make the Android playground app compatible with AGP 8.x

--- playground/app/build.gradle.kts
+++ playground/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
   alias(libs.plugins.android.application)
   alias(libs.plugins.kotlin.compose)
+  alias(libs.plugins.kotlin.android)
 }
 
 android {
@@ -31,6 +32,9 @@ android {
   }
   buildFeatures {
     compose = true
+  }
+  kotlinOptions {
+    jvmTarget = "11"
   }
 }
 
