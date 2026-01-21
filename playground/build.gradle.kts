0c9d9f1942f900a676a54d5640703137584c609b
Reza Moallemi <rm.allmails@gmail.com>
2026-01-20 20:16:46 -0800

    Upgrade playground app to AGP 9.0.0

--- playground/build.gradle.kts
+++ playground/build.gradle.kts
@@ -13,5 +13,4 @@ buildscript {
 plugins {
   alias(libs.plugins.android.application) apply false
   alias(libs.plugins.kotlin.compose) apply false
-  alias(libs.plugins.kotlin.android) apply false
 }
